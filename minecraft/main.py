6360399f26af9cd08e1e8fc4f96f1e32f3113956
v1cferr <dev.victorferreira@gmail.com>
2025-11-23 20:40:05 -0300

    feat: Add initial script idea for automating Minecraft mod filtering between client and server.

---
+++ minecraft/main.py
@@ -0,0 +1,9 @@
+# Rascunho/Ideia para automatizar:
+# Script para automatizar o filtro de mods entre client/servidor de modpacks do CurseForge | Minecraft Java Edition
+
+# Exemplo de diretório de mods do cliente: /home/v1cferr/Documents/curseforge/minecraft/Instances/Study/mods
+# Exemplo de diretório de mods do servidor: /home/v1cferr/minecraft-server/forge/mods
+
+# - Usar a API do Gemini para verificar o log ao executar o servidor e excluir eventuais mods que rodam apenas no client
+# - Sempre executar o run.sh ou run.bat (verificar qual o SO, Linux ou Windows) do servidor para verificar se está abrindo o servidor normalmente
+# -- ou dando conflitos com mods que apenas rodam no client
